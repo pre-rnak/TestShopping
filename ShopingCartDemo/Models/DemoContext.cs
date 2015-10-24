7114687e635a373811b90b20ca743099dafb56d2
Prerna <prerna.khandelwal@wwindia.com>
2015-10-24 12:16:19 +0530

    Sopping cart

---
+++ ShopingCartDemo/Models/DemoContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace ShopingCartDemo.Models
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext()
+        {
+            Database.SetInitializer<DemoContext>(null);
+        }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Login> Login { get; set; }
+    }
+}
